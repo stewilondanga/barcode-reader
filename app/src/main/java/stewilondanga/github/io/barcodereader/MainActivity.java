f855627f7dedf08b72c4f0ff0e7ade7ef89dee6d
Stewart ilondanga <stilondanga@gmail.com>
2019-08-16 21:32:57 +0300

    add app/src/main/java/stewilondanga/github/io/barcodereader/MainActivity.java

--- app/src/main/java/stewilondanga/github/io/barcodereader/MainActivity.java
+++ app/src/main/java/stewilondanga/github/io/barcodereader/MainActivity.java
@@ -1,1 +1,12 @@
 package stewilondanga.github.io.barcodereader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
