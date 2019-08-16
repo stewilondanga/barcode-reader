1e4e1add8c0615ed56feb21ee1fd67ff57314268
Stewart ilondanga <stilondanga@gmail.com>
2019-08-16 22:02:45 +0300

    addapp/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java

--- app/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
+++ app/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
@@ -7,6 +7,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
 
 public class IntentIntegrator {
 }
