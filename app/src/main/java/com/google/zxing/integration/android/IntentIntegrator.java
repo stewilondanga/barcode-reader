a6a6c09553f6ef510493cc0c4057ac7c759b0085
Stewart ilondanga <stilondanga@gmail.com>
2019-08-16 23:41:42 +0300

    addapp/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java

--- app/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
+++ app/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
@@ -19,11 +19,13 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class IntentIntegrator {
-    public static final int REQUEST_CODE = 0x0000c0de; //Only use bottom 16 bits
+
+    public static final int REQUEST_CODE = 0x0000c0de; // Only use bottom 16 bits
     private static final String TAG = IntentIntegrator.class.getSimpleName();
 
     public static final String DEFAULT_TITLE = "Install Barcode Scanner?";
-    public static final String DEFAULT_MESSAGE = "This application requires Barcode Scanner. Would you like to install it?";
+    public static final String DEFAULT_MESSAGE =
+            "This application requires Barcode Scanner. Would you like to install it?";
     public static final String DEFAULT_YES = "Yes";
     public static final String DEFAULT_NO = "No";
 
@@ -32,7 +34,9 @@ public class IntentIntegrator {
 
     // supported barcode formats
     public static final Collection<String> PRODUCT_CODE_TYPES = list("UPC_A", "UPC_E", "EAN_8", "EAN_13", "RSS_14");
-    public static final Collection<String> ONE_D_CODE_TYPES = list("UPC_A", "UPC_E", "EAN_8", "EAN_13", "CODE_39", "CODE_93", "CODE_128", "ITF", "RSS_14", "RSS_EXPANDED");
+    public static final Collection<String> ONE_D_CODE_TYPES =
+            list("UPC_A", "UPC_E", "EAN_8", "EAN_13", "CODE_39", "CODE_93", "CODE_128",
+                    "ITF", "RSS_14", "RSS_EXPANDED");
     public static final Collection<String> QR_CODE_TYPES = Collections.singleton("QR_CODE");
     public static final Collection<String> DATA_MATRIX_TYPES = Collections.singleton("DATA_MATRIX");
 
@@ -52,62 +56,63 @@ public class IntentIntegrator {
     private String buttonYes;
     private String buttonNo;
     private List<String> targetApplications;
-    private final Map<String, Object> moreExtras;
+    private final Map<String,Object> moreExtras;
 
     public IntentIntegrator(Activity activity) {
         this.activity = activity;
         title = DEFAULT_TITLE;
-        message = DEFAULT_YES;
+        message = DEFAULT_MESSAGE;
+        buttonYes = DEFAULT_YES;
         buttonNo = DEFAULT_NO;
         targetApplications = TARGET_ALL_KNOWN;
-        moreExtras = new HashMap<String, Object>(3);
+        moreExtras = new HashMap<String,Object>(3);
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title){
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setTitleBYID(int titleID){
+    public void setTitleByID(int titleID) {
         title = activity.getString(titleID);
     }
 
-    public String getMessage(){
+    public String getMessage() {
         return message;
     }
 
-    public void setMessage(String message){
+    public void setMessage(String message) {
         this.message = message;
     }
 
-    public void setMessageByID(int messageID){
+    public void setMessageByID(int messageID) {
         message = activity.getString(messageID);
     }
 
-    public String getButtonYes(){
+    public String getButtonYes() {
         return buttonYes;
     }
 
-    public void setButtonYes(String buttonYes){
+    public void setButtonYes(String buttonYes) {
         this.buttonYes = buttonYes;
     }
 
-    public void setButtonYesByID(int buttonYesID){
+    public void setButtonYesByID(int buttonYesID) {
         buttonYes = activity.getString(buttonYesID);
     }
 
-    public String getButtonNo(){
+    public String getButtonNo() {
         return buttonNo;
     }
 
-    public void setButtonNo(String buttonNo){
+    public void setButtonNo(String buttonNo) {
         this.buttonNo = buttonNo;
     }
 
-    public void setButtonNoByID(int buttonNoID){
+    public void setButtonNoByID(int buttonNoID) {
         buttonNo = activity.getString(buttonNoID);
     }
 
@@ -115,18 +120,18 @@ public class IntentIntegrator {
         return targetApplications;
     }
 
-    public final void setTargetApplications(List<String> targetApplications){
-        if (targetApplications.isEmpty()){
+    public final void setTargetApplications(List<String> targetApplications) {
+        if (targetApplications.isEmpty()) {
             throw new IllegalArgumentException("No target applications");
         }
         this.targetApplications = targetApplications;
     }
 
-    public void setSingleTargetApplication(String targetApplication){
+    public void setSingleTargetApplication(String targetApplication) {
         this.targetApplications = Collections.singletonList(targetApplication);
     }
 
-    public Map<String, ?> getMoreExtras(){
+    public Map<String,?> getMoreExtras() {
         return moreExtras;
     }
 
@@ -134,20 +139,21 @@ public class IntentIntegrator {
         moreExtras.put(key, value);
     }
 
-    public final AlertDialog initiateScan(){
+
+    public final AlertDialog initiateScan() {
         return initiateScan(ALL_CODE_TYPES);
     }
 
-    public final AlertDialog initiateScan(Collection<String> desiredBarcodeFormats){
+    public final AlertDialog initiateScan(Collection<String> desiredBarcodeFormats) {
         Intent intentScan = new Intent(BS_PACKAGE + ".SCAN");
         intentScan.addCategory(Intent.CATEGORY_DEFAULT);
 
         // check which types of codes to scan for
         if (desiredBarcodeFormats != null) {
-            //set the desired barcode types
+            // set the desired barcode types
             StringBuilder joinedByComma = new StringBuilder();
             for (String format : desiredBarcodeFormats) {
-                if  (joinedByComma.length() > 0) {
+                if (joinedByComma.length() > 0) {
                     joinedByComma.append(',');
                 }
                 joinedByComma.append(format);
@@ -155,17 +161,19 @@ public class IntentIntegrator {
             intentScan.putExtra("SCAN_FORMATS", joinedByComma.toString());
         }
 
+
         String targetAppPackage = findTargetAppPackage(intentScan);
         if (targetAppPackage == null) {
             return showDownloadDialog();
         }
         intentScan.setPackage(targetAppPackage);
         intentScan.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intentScan.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK_RESET);
+        intentScan.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
         attachMoreExtras(intentScan);
         startActivityForResult(intentScan, REQUEST_CODE);
         return null;
     }
+
 
     protected void startActivityForResult(Intent intent, int code) {
         activity.startActivityForResult(intent, code);
@@ -174,10 +182,10 @@ public class IntentIntegrator {
     private String findTargetAppPackage(Intent intent) {
         PackageManager pm = activity.getPackageManager();
         List<ResolveInfo> availableApps = pm.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-        if (availableApps != null){
+        if (availableApps != null) {
             for (ResolveInfo availableApp : availableApps) {
                 String packageName = availableApp.activityInfo.packageName;
-                if (targetApplications.contains(packageName)){
+                if (targetApplications.contains(packageName)) {
                     return packageName;
                 }
             }
@@ -185,5 +193,93 @@ public class IntentIntegrator {
         return null;
     }
 
-    private
+    private AlertDialog showDownloadDialog() {
+        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(activity);
+        downloadDialog.setTitle(title);
+        downloadDialog.setMessage(message);
+        downloadDialog.setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String packageName = targetApplications.get(0);
+                Uri uri = Uri.parse("market://details?id=" + packageName);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                try {
+                    activity.startActivity(intent);
+                } catch (ActivityNotFoundException anfe) {
+                    // Hmm, market is not installed
+                    Log.w(TAG, "Google Play is not installed; cannot install " + packageName);
+                }
+            }
+        });
+        downloadDialog.setNegativeButton(buttonNo, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {}
+        });
+        return downloadDialog.show();
+    }
+
+    public static IntentResult parseActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String formatName = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                byte[] rawBytes = intent.getByteArrayExtra("SCAN_RESULT_BYTES");
+                int intentOrientation = intent.getIntExtra("SCAN_RESULT_ORIENTATION", Integer.MIN_VALUE);
+                Integer orientation = intentOrientation == Integer.MIN_VALUE ? null : intentOrientation;
+                String errorCorrectionLevel = intent.getStringExtra("SCAN_RESULT_ERROR_CORRECTION_LEVEL");
+                return new IntentResult(contents,
+                        formatName,
+                        rawBytes,
+                        orientation,
+                        errorCorrectionLevel);
+            }
+            return new IntentResult();
+        }
+        return null;
+    }
+
+    public final AlertDialog shareText(CharSequence text, CharSequence type) {
+        Intent intent = new Intent();
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setAction(BS_PACKAGE + ".ENCODE");
+        intent.putExtra("ENCODE_TYPE", type);
+        intent.putExtra("ENCODE_DATA", text);
+        String targetAppPackage = findTargetAppPackage(intent);
+        if (targetAppPackage == null) {
+            return showDownloadDialog();
+        }
+        intent.setPackage(targetAppPackage);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        attachMoreExtras(intent);
+        activity.startActivity(intent);
+        return null;
+    }
+
+    private static List<String> list(String... values) {
+        return Collections.unmodifiableList(Arrays.asList(values));
+    }
+
+    private void attachMoreExtras(Intent intent) {
+        for (Map.Entry<String,Object> entry : moreExtras.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            // Kind of hacky
+            if (value instanceof Integer) {
+                intent.putExtra(key, (Integer) value);
+            } else if (value instanceof Long) {
+                intent.putExtra(key, (Long) value);
+            } else if (value instanceof Boolean) {
+                intent.putExtra(key, (Boolean) value);
+            } else if (value instanceof Double) {
+                intent.putExtra(key, (Double) value);
+            } else if (value instanceof Float) {
+                intent.putExtra(key, (Float) value);
+            } else if (value instanceof Bundle) {
+                intent.putExtra(key, (Bundle) value);
+            } else {
+                intent.putExtra(key, value.toString());
+            }
+        }
+    }
 }
