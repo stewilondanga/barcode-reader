4b6fd23cf17fb5e193a9b859f9da0a6f587aeb25
Stewart ilondanga <stilondanga@gmail.com>
2019-08-16 22:44:14 +0300

    addapp/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java

--- app/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
+++ app/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
@@ -94,4 +94,10 @@ public class IntentIntegrator {
     public void setButtonYes(String buttonYes){
         this.buttonYes = buttonYes;
     }
+
+    public void setButtonYesByID(int buttonYesID){
+        buttonYes = activity.getString(buttonYesID);
+    }
+
+
 }
