18bd0856e4b3cca840ed07bbb319484bdd3c999b
Stewart ilondanga <stilondanga@gmail.com>
2019-08-16 21:58:18 +0300

    addapp/src/main/java/com/google/zxing/integration/android/IntentINtegrator.java

--- app/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
+++ app/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
@@ -1,4 +1,12 @@
 package com.google.zxing.integration.android;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class IntentIntegrator {
 }
